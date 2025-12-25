f0236de1094c605ef74da5e3ef6d40a3539618fb
boroeun <boroeun.khem6@gmail.com>
2025-12-25 13:44:32 +0700

    add cals

--- p.py
+++
@@ -1,8 +0,0 @@
-print("This is file p.py")
-print("Hello from p.py!")
-print("End of p.py")
-print("End of p")
-print("Hi this is the newF")
-print("this is the second change in p.py") 
-print("Final change in p.py")
-print("this is in new branch")
